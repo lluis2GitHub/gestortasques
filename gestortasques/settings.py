38317533c427ccecef219e71acc8030d3947c727
lluis2GitHub <55973460+lluis2GitHub@users.noreply.github.com>
2026-09-10 21:16:47 +0200

    Add setting

--- gestortasques/settings.py
+++ gestortasques/settings.py
@@ -85,13 +85,6 @@ WSGI_APPLICATION = 'gestortasques.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
