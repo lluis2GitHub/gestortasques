e6c7e49181fe80115181689408f9c7bc1fc9a7ed
lluis2GitHub <55973460+lluis2GitHub@users.noreply.github.com>
2026-09-10 19:40:04 +0200

    Configurar PostgreSQL

--- gestortasques/settings.py
+++ gestortasques/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'gestortasques.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestortasques',
+        'USER': 'postgres',
+        'PASSWORD': 'fantastic',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
